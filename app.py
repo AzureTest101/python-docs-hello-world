c64d3955ae3678a30a98c8c3bd997446d54450b5
AzureTest101 <140730058+AzureTest101@users.noreply.github.com>
2023-08-02 15:55:50 +0100

    11
    12

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    print('Request for index page received')
-   return render_template('auth.html')
+   return render_template('test.html')
 
 @app.route('/favicon.ico')
 def favicon():
